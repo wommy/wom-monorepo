b46b2e2d6bb91e028691a40fb05e8cbaf009f40f
wommy <tom@technomad.media>
2022-05-11 05:40:01 -0400

    rough draft done

--- packages/page-marx/_data/marx.js
+++ packages/page-marx/_data/marx.js
@@ -13,7 +13,7 @@ const urlMarx = `${process.env.sanity_url_full}${encodeURIComponent(query)}`
 module.exports = async function () {
 	const { result: marxPre } = await EleventyFetch(urlMarx)
 
-	console.log('==========')
+	// console.log('==========')
 	// const foo = marxPre[0]
 	// console.log(foo._createdAt)
 	// console.log('----------')
@@ -25,9 +25,7 @@ module.exports = async function () {
 	// console.log('----------')
 	const marx2021 = marxPre.filter( x => x._createdAt.split('-')[0] === '2021' )
 	const marx2022 = marxPre.filter( x => x._createdAt.split('-')[0] === '2022' )
-	// console.log( marx2021.length, ',', marx2022.length )
-	// console.log('----------')
-
+	// console.log( ' --- articles : ', marx2021.length, ',', marx2022.length )
 	// console.log( marx2021[0] )
 	// const monthsARR = marxPre.reduce( x => x._createdAt.split('-')[1], [] )
 		// console.log(
@@ -40,16 +38,102 @@ module.exports = async function () {
 	// marx2021.indexOf(x._createdAt.split('-')[1]) === i
 
 // reduce
-	// const monthsARR = marx2021.reduce( ( acc, x ) => {
-	// 	const month = x._createdAt.split('-')[1]
-	// 	return acc.includes(month) ? acc : [...acc, month] 
-	// }, [])
+	const monthsARR = marxPre.reduce( ( acc, x ) => {
+		const yearMonth = x._createdAt.split('-')[0] + '-' + x._createdAt.split('-')[1]
+		// const month = x._createdAt.split('-')[1]
+		return acc.includes(yearMonth) ? acc : [...acc, yearMonth] 
+	}, [])
+	// console.log(' --- year/month : ', monthsARR)
 	// console.log( monthsARR )
+	let marxPost = {}
+	monthsARR.forEach( x => {
+		marxPost[x] = []
+	} )
 
+	// console.log(marxPost)
 
-	console.log('- months -')
+	// console.log(marxPre[0])
+	marxPre.forEach( ea => {
+		let entry = {}
+		// entry.date = ea._createdAt.split('T')[0]
+		// entry.time = ea._createdAt.split('T')[1]
+		const [year,month,day] = ea._createdAt.split('T')[0].split('-')
+		const yearMonth = year+'-'+month
+		entry.title = ea.title
+		entry.url = ea.url
+		entry.datetime = ea._createdAt
+		entry.category = ea.category
+
+		// foo[yearMonth].push(yearMonthDay+'--'+ea.title) 
+		marxPost[yearMonth].push(entry) 
+	})
+	// console.log(' --- year/month : ', monthsARR)
+	// console.log( monthsARR )
+	// let foo = {}
+	// monthsARR.forEach( x => {
+	// 	foo[x] = []
+	// } )
+
+	// Object.keys(marxPost).forEach( ea => console.log(ea+': '+marxPost[ea].length ) )
+
+	// console.log(marxPost['2021-09'].length)
+
+	// marxPre.forEach( x => {
+	// 	const [ year, month ] = x._createdAt.split('-')
+	// 	console.log( year+'-'+month )
+	// })
+
 	// const marx = [ marx2022, marx2021 ]
 	// console.log( marx )
-	console.log('==========')
-	// return { marx }
+	// console.log('==========')
+	return marxPost
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
