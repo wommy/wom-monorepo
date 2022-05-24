9936a7183433d3d3b7f50781b2d862129b7363f1
wommy <tom@technomad.media>
2022-05-23 22:20:39 -0400

    prep for mv

--- packages/page-marx/utils/marx.js
+++ packages/page-marx/utils/marx.js
@@ -1,19 +1,16 @@
 require('dotenv').config()
-const sanitize = groq => 
-	process.env.sanity_path + encodeURIComponent(
-		groq.replace(/\s/g, '')
-	)
+export const sanitize = groq =>
+	process.env.sanity_path + encodeURIComponent(groq.replace(/\s/g, ''))
 
-const fetch = (url, duration = '1d', type = 'json') =>
-	require('@11ty/eleventy-fetch')( sanitize(url), { duration, type })
+export const cache = (url, duration = '1d', type = 'json') =>
+	require('@11ty/eleventy-fetch')(sanitize(url), { duration, type })
 
-const marx = `*[ _type=='bookmark' ] | order( _createdAt )`,
-	extend = ext => `*[ _type=='bookmark' ${ext||''} ] | order( _createdAt )`,
-	since = time => extend(`&& dateTime( _updatedAt ) > dateTime( now() ) - ${time}`)
-	hour = 60*60,
-	day = hour*24,
-	week = day*7
-	// query = marx
-;
+// const extend = ext =>
+// 	`*[ _type=='bookmark' ${ext || ''} ] | order( _createdAt )`
+// const since = time =>
+// 	extend(`&& dateTime( _updatedAt ) > dateTime( now() ) - ${time}`)
+// const hour = 60 * 60,
+// 	day = hour * 24,
+// 	week = day * 7
 
-fetch( since( week ) ).then( x => console.log(x) )
+// const marx_since = time => cache(since(time)).then(x => console.log(x.result))
