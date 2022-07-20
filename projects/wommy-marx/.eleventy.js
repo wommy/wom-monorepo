f3ed5123bd4280855368c6d4fc9b19592a7227d7
wommy <tom@technomad.media>
2022-07-19 23:44:45 -0400

    proj: wommy-marx

---
+++ projects/wommy-marx/.eleventy.js
@@ -0,0 +1,7 @@
+/**
+ *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ *  @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin( require('@11ty/eleventy').EleventyRenderPlugin )
+}
