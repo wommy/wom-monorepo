6402f015c7fe9db1d556e87a3b5b33c294666cae
wommy <tom@technomad.media>
2022-07-20 03:10:55 -0400

    chore: _layouts: split out

--- projects/wommy-marx/.eleventy.js
+++ projects/wommy-marx/.eleventy.js
@@ -3,5 +3,12 @@
  *  @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
  */
 module.exports = function(eleventyConfig) {
+
 	eleventyConfig.addPlugin( require('@11ty/eleventy').EleventyRenderPlugin )
+
+	return {
+		dir: {
+			layouts: "_layouts"
+		}
+	}
 }
