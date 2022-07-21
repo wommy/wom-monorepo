e7fb200dd3e37849b19710f86d82f3be8b01bcae
wommy <tom@technomad.media>
2022-07-21 17:57:29 -0400

    pack: api-sanity--marx-get

---
+++ packages/api-sanity/marx_get/index.js
@@ -0,0 +1,10 @@
+require('@sanity/client')({
+	projectId: '4mxx5zpk',
+	dataset: 'production',
+	apiVersion: '2022-07-21',
+	token:
+		'sk7axbQaIMRo8P6Y8Hx5RFGu4MY7w1bNOeD5uTujlr5pLRIUGypkpVlmaSfwwmwTuWnOu5ZAzwv6IeIwM5yUPwQe4jwaIgoYzX6J3mLkbNdsfjibYmUgQKzaP3DTCBeK0ngcGEUoj8CPZlFfWio11jLwM1eiVorhBQhd2vJVmDkUoHoQQaG3',
+	useCdn: true,
+})
+	.fetch(`*[_type=='bookmark']|order(_createdAt desc)`)
+	.then( marx => console.log(marx) )
