ea00fd87c8696e653f6c24b8f041147e5d8a13f2
wommy <tom@technomad.media>
2022-07-22 02:51:24 -0400

    style: globalData getSanityMarx

--- packages/api/sanity-marx-get/index.js
+++ packages/api/sanity-marx-get/index.js
@@ -1,9 +1,8 @@
-module.exports = async () =>
+module.exports = async (query, params = {}) =>
 	await require('@sanity/client')({
 		projectId: '4mxx5zpk',
 		dataset: 'production',
 		apiVersion: '2022-07-21',
-		token:
-			'sk7axbQaIMRo8P6Y8Hx5RFGu4MY7w1bNOeD5uTujlr5pLRIUGypkpVlmaSfwwmwTuWnOu5ZAzwv6IeIwM5yUPwQe4jwaIgoYzX6J3mLkbNdsfjibYmUgQKzaP3DTCBeK0ngcGEUoj8CPZlFfWio11jLwM1eiVorhBQhd2vJVmDkUoHoQQaG3',
+		token: 'sk7axbQaIMRo8P6Y8Hx5RFGu4MY7w1bNOeD5uTujlr5pLRIUGypkpVlmaSfwwmwTuWnOu5ZAzwv6IeIwM5yUPwQe4jwaIgoYzX6J3mLkbNdsfjibYmUgQKzaP3DTCBeK0ngcGEUoj8CPZlFfWio11jLwM1eiVorhBQhd2vJVmDkUoHoQQaG3',
 		useCdn: true,
-	}).fetch(`*[_type=='bookmark']|order(_createdAt desc)`)
+	}).fetch(query, params)
--- packages/pages/marx/.eleventy.js
+++ packages/pages/marx/.eleventy.js
@@ -2,11 +2,14 @@
  *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  *  @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
  */
-module.exports = function(eleventyConfig) {
-
-	eleventyConfig.addPlugin( require('@11ty/eleventy').EleventyRenderPlugin )
-
-	// eleventyConfig.addGlobalData('sanity_marx_get', require('../../api/sanity-marx-get'))
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(require('@11ty/eleventy').EleventyRenderPlugin)
+	// eleventyConfig.addGlobalData(
+	// 	'sanity_marx_get',
+	// 	require('../../api/sanity-marx-get')(
+	// 		`*[_type=='bookmark']|order(_createdAt desc)`,
+	// 	),
+	// )
 
 	return {
 		pathPrefix: 'marx/',
