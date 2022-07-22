dd7c219d0c90ea1707ec4095ea0c96bab7af332e
wommy <tom@technomad.media>
2022-07-22 00:05:51 -0400

    style: .11ty.js syntax

--- packages/pages/marx/index.11ty.js
+++ packages/pages/marx/index.11ty.js
@@ -1,10 +1,11 @@
-module.exports.data = {
-	layout: 'with_header/index.html',
-	list: ['apple', 'banana', 'carrot', 'donut'],
-}
-
-module.exports.render = async function ({ list }) {
-	const incl_path = x => `./_includes/${x}/index.11ty.js`
-	// return await this.renderFile(incl_path('list_data'))
-	return await this.renderFile(incl_path('list'), list)
+module.exports = {
+	data: {
+		layout: 'with_header/index.html',
+		list: ['apple', 'banana', 'carrot', 'donut'],
+	},
+	async render({ list }) {
+		const incl_path = x => `./_includes/${x}/index.11ty.js`
+		// return await this.renderFile(incl_path('list_data'))
+		return await this.renderFile(incl_path('list'), list)
+	},
 }
