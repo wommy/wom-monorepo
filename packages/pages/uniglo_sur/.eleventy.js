dc4688864333ab1f6e7a893950cb668ddef06662
wommy <tom@technomad.media>
2022-07-26 19:29:23 -0400

    init: sammy uniglo

---
+++ packages/pages/uniglo_sur/.eleventy.js
@@ -0,0 +1,29 @@
+/**
+ *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ *  @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function (eleventyConfig) {
+	eleventyConfig.setQuietMode(true)
+	eleventyConfig.addPlugin(require('@11ty/eleventy').EleventyRenderPlugin)
+	eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-vite'), {
+		viteOptions: {
+			appType: 'custom',
+			server: { middlewareMode: true },
+		},
+	})
+	eleventyConfig.addGlobalData('getSanityMarx', async () =>
+		Promise.resolve(
+			// require('../../api/sanity-marx-get/index')(
+			// 	`*[_type=='bookmark']|order(_createdAt)`,
+			// ),
+		),
+	)
+
+	return {
+		// pathPrefix: 'sammy_uniglo/',
+		dir: {
+			includes: '../../components',
+			layouts: '../../layouts',
+		},
+	}
+}
